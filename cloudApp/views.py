2e96deb827dac6eab171ecd9197f9c7086df6f9c
Andrei <aa15g14@soton.ac.uk>
2017-06-26 16:58:15 +0100

    Changed the views mb the bug is there

--- cloudApp/views.py
+++ cloudApp/views.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.http import HttpResponse
+#from django.shortcuts import render
+django.http import HttpResponse
 
-# Create your views here.
 def index(request):
-	return HttpResponse("Hello, world. You're at cascoda's index.")
+    return HttpResponse("Hello, world. You're at the polls index.")
