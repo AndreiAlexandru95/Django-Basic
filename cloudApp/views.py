966bbe1dd61a10d6b5b82b8844f84fa39e25c75f
Andrei <aa15g14@soton.ac.uk>
2017-06-29 11:53:08 +0100

    Fixed small bug, readded index to views

--- cloudApp/views.py
+++ cloudApp/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+	return HttpResponse("Hello, world!")
+
 def detail(request, question_id):
 	return HttpResponse("You're looking at question %s." % question_id)
 
