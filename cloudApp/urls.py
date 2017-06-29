54d3ebf165c437e26c5a4d5f2b3fec8e09a4d2cd
Andrei <aa15g14@soton.ac.uk>
2017-06-29 15:16:01 +0100

    Amended URLconf and views

--- cloudApp/urls.py
+++ cloudApp/urls.py
@@ -2,14 +2,22 @@ from django.conf.urls import url
 
 from . import views
 
+#app_name = 'cloudApp'
+#urlpatterns = [
+#    # ex: /cloudApp/
+#    url(r'^$', views.index, name='index'),
+#    # ex: /cloudApp/5/
+#    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+#    # ex: /cloudApp/5/results/
+#    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+#    # ex: /cloudApp/5/vote/
+#    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+#]
+
 app_name = 'cloudApp'
 urlpatterns = [
-    # ex: /cloudApp/
-    url(r'^$', views.index, name='index'),
-    # ex: /cloudApp/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # ex: /cloudApp/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /cloudApp/5/vote/
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+	url(r'^$', views.IndexView.as_view(), name = 'index'),
+	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name = 'detail'),
+	url(r'^(?P<pk>[0-9]+)/results/$'), views.ResultsView.as_view(), name = 'results'),
+	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- cloudApp/views.py
+++ cloudApp/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.db.models import F
+from django.views import generic
 #from django.template import loader
 #from django.http import Http404
 
@@ -12,6 +13,8 @@ from .models import Choice, Question
 
 # Create your views here.
 
+
+# Commit 1
 #def index(request):
 #	#Display the latest 5 poll questions in the system 
 #	latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -21,12 +24,14 @@ from .models import Choice, Question
 #	}
 #	return HttpResponse(template.render(context, request))
 
+# Commit 2
 # Short version of the above - without using django.template
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	context = {'latest_question_list': latest_question_list}
-	return render(request, 'cloudApp/index.html', context)
+#def index(request):
+#	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#	context = {'latest_question_list': latest_question_list}
+#	return render(request, 'cloudApp/index.html', context)
 
+# Commit 1
 #def detail(request, question_id):
 #	try:
 #		question = Question.objects.get(pk = question_id)
@@ -34,14 +39,32 @@ def index(request):
 #		raise Http404("Question does not exist")
 #	return render(request, 'cloudApp/detail.html', {'question': question})
 
+# Commit 2
 # Shortcut version of the above
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk = question_id)
-	return render(request, 'cloudApp/detail.html', {'question': question})
+#def detail(request, question_id):
+#	question = get_object_or_404(Question, pk = question_id)
+#	return render(request, 'cloudApp/detail.html', {'question': question})
 
-def results(request, question_id):
-	question = get_object_or_404(Question, pk = question_id)
-	return render(request, 'cloudApp/results.html', {'question': question})
+#def results(request, question_id):
+#	question = get_object_or_404(Question, pk = question_id)
+#	return render(request, 'cloudApp/results.html', {'question': question})
+
+class IndexView(generic.ListView):
+	template_name = 'cloudApp/index.html'
+	context_object_name = 'latest_question_list'
+
+	#Return the last five published questions
+	def get_queryset(self):
+		return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'cloudApp/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk = question_id)
