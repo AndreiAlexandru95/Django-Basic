7fb7b07483ece3b2d956d68f93b39ad8153caf16
Andrei <aa15g14@soton.ac.uk>
2017-06-29 11:49:44 +0100

    Wired the prev added views with URLs

--- cloudApp/urls.py
+++ cloudApp/urls.py
@@ -3,5 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex: /cloudApp/
     url(r'^$', views.index, name='index'),
+    # ex: /cloudApp/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /cloudApp/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /cloudApp/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
