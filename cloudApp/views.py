45950f01196de4db177f97d8c62a658b7ecda136
Andrei <aa15g14@soton.ac.uk>
2017-06-29 12:14:40 +0100

    Fix small bug for prev push - add import Question in views

--- cloudApp/views.py
+++ cloudApp/views.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 # Create your views here.
 
 def index(request):
