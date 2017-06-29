6a3c45d809f6fee02b089ae68f6a78214c8af25c
Andrei <aa15g14@soton.ac.uk>
2017-06-29 13:34:13 +0100

    Added Http404 Import to views to fix bug

--- cloudApp/views.py
+++ cloudApp/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 #from django.template import loader
+from django.http import Http404
 
 from .models import Question
 
