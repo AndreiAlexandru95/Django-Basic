718ae0e45aa16af32c9e2226009e278fefe800d7
Andrei <aa15g14@soton.ac.uk>
2017-06-29 13:26:27 +0100

    Added a shortcut to the prev commit

--- cloudApp/views.py
+++ cloudApp/views.py
@@ -3,20 +3,26 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+#from django.template import loader
 
 from .models import Question
 
 # Create your views here.
 
+#def index(request):
+#	#Display the latest 5 poll questions in the system 
+#	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#	template = loader.get_template('cloudApp/index.html')
+#	context = {
+#		'latest_question_list': latest_question_list,
+#	}
+#	return HttpResponse(template.render(context, request))
+
+# Short version of the above - without using django.template
 def index(request):
-	#Display the latest 5 poll questions in the system 
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('cloudApp/index.html')
-	context = {
-		'latest_question_list': latest_question_list,
-	}
-	return HttpResponse(template.render(context, request))
+	context = {'latest_question_list': latest_question_list}
+	return render(request, 'cloudApp/index.html', context)
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at question %s." % question_id)
